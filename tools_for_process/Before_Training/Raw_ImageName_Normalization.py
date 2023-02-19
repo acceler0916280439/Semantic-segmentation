12f0958c5643b4e0aaa3a95d39522d10201cbabf
Chris Huang <43138129+acceler0916280439@users.noreply.github.com>
2023-02-19 11:23:27 +0800

    Update and rename useful_tools/Before_Training/Raw_ImageName_Normalization.py to tools_for_process/Before_Training/Raw_ImageName_Normalization.py

--- tools_for_process/Before_Training/Raw_ImageName_Normalization.py
+++ tools_for_process/Before_Training/Raw_ImageName_Normalization.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 import os
 from PIL import Image
 
-
-# In[ ]:
 
 
 path = input('Input your raw picture path: ')
@@ -21,8 +17,6 @@ img_file = os.listdir(origin_path)
 file_amount = len(img_file)
 
 
-# In[ ]:
-
 
 for i in range(file_amount):
     Input = Image.open(origin_path + img_file[i])
@@ -30,9 +24,6 @@ for i in range(file_amount):
     print('Success to transform the picture name into %d-1_Color.png'%(i+1))
     
 print('Finished!')
-
-
-# In[ ]:
 
 
 
